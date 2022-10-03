4e1963e025b0af94b86c0c8554065d0659a3e245
fauzan098 <muhammadariffauzan03@gmail.com>
2022-10-03 11:23:34 +0700

    routing

--- app/core/App.php
+++ app/core/App.php
@@ -3,6 +3,20 @@
 class App {
     public function __construct()
     {
-        echo 'mantap';
+        $url = $this->parseURL();
+        var_dump($url);
+    }
+
+    public function parseURL() 
+    {
+        if (isset($_GET['url']) ) {
+            // fungi rtrim digunakan untuk dapat menghapus spasi atau karakter standar lainnya dari sisi kanan string (disini yang akan dihapus "/")
+            $url = rtrim($_GET['url'], '/');
+            // membersihkan karakter aneh pada url menggunakan parameter filter sanitize
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            // memcahkan url dengan fungsi explode
+            $url = explode('/', $url);
+            return $url;
+        }
     }
 }
