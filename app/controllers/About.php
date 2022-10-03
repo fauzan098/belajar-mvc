dc3f5f847fd9441fb6143d42c837a236878e6d20
fauzan098 <muhammadariffauzan03@gmail.com>
2022-10-03 16:46:01 +0700

    controller

---
+++ app/controllers/About.php
@@ -0,0 +1,12 @@
+<?php 
+
+class About {
+    public function index($nama, $makanan)
+    {
+        echo "halo saya $nama saya suka $makanan";
+    }
+    public function page() 
+    {
+        echo 'About/page';
+    }
+}
